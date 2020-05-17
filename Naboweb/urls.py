d4caa2a691ababfeba4af1053bb31adc5fc16ab2
Victor <vicferron@hotmail.es>
2020-05-17 12:36:53 +0200

    intento diez millones4434

--- Naboweb/urls.py
+++ Naboweb/urls.py
@@ -14,14 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from Naboform import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('form/', views.Naboform, name='form'),
     path('contacto/', views.contacto, name='contacto'),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    re_path(r'^%s(?P<path>.*)$' % "static/", serve, kwargs={'document_root': settings.STATIC_ROOT}),
+]

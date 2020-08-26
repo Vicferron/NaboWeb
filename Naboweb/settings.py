991f742a135b4b64933ef90fbd8b44aa9ebf7152
Vicferron <65445340+Vicferron@users.noreply.github.com>
2020-08-26 21:10:05 +0200

    Update settings.py

--- Naboweb/settings.py
+++ Naboweb/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cbt42m86q4#k9-l@!0vdh71jd#xa(7b&*swd94x$f@w$i2s1(2'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = strtobool(os.environ.get("DEBUG", 'True'))
